dad6a76850abe94e70b34badb300b320cae85748
imad <imad.salki@hotmail.fr>
2021-12-07 20:46:29 +0100

    Update

---
+++ picture-in-picture/script.js
@@ -0,0 +1,31 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+
+// Prompt to select a media stream, pass to video element then play
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+
+    } catch (error) {
+        console.log(`Une erreur a été rencontrée : ${error}`);
+    }
+}
+
+button.addEventListener('click', async () => {
+
+    // Disable button
+    button.disabled = true;
+
+    // Start Picture in picture
+    await videoElement.requestPictureInPicture();
+
+    // Reset button
+    button.disabled = false;
+});
+
+selectMediaStream();
